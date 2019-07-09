1b101861b0482c1c8193ff28a4e8f9488faecbb3
Ehshan <ehshan@msn.com>
2019-07-09 21:10:16 +0000

    added function to amazon data from remote source

--- product_classifier.py
+++ product_classifier.py
@@ -5,8 +5,10 @@
 #%%
 #%%
 '''Import Dependencies'''
+
 from sklearn.model_selection import train_test_split
 import pandas as pd
+import numpy as np
 import tensorflow as tf
 import tensorflow_hub as hub
 from datetime import datetime
@@ -15,6 +17,7 @@ from datetime import datetime
 
 #%%
 '''Import BERT'''
+
 import bert
 from bert import run_classifier
 from bert import optimization
@@ -22,6 +25,43 @@ from bert import tokenization
 
 #%%
 '''Download and Load Data Function'''
+
 from tensorflow import keras
 import os
 import re
+
+
+#%%
+'''Set output directory for model and eval files '''
+
+train_out = 'rain_out'
+OUTPUT_DIR = train_out
+
+#%%
+
+'''Load data from remote into dataframe '''
+
+def load_data_from_remote(force_download = False):
+    
+    # load data from url
+    dataset = tf.keras.utils.get_file(
+        fname = "sample_us.tsv", 
+        origin = "https://s3.amazonaws.com/amazon-reviews-pds/tsv/sample_us.tsv", 
+        extract = False)
+    
+    # relevant fields from data
+    fields = ['review_id', 'star_rating', 'review_body']
+    
+    # read data to df
+    df = pd.read_csv(dataset, sep='\t', header=0, skipinitialspace=True, usecols=fields)
+
+    return df
+
+#%%
+'''Spit data to train/test sets '''
+
+df = load_data_from_remote()
+train, test = train_test_split(df, test_size=0.2)
+
+
+#%%
