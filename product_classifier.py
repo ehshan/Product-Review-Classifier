e77f42cf21fe8d099e3fd6f9b9ef3bdc16860129
Ehshan <ehshan@msn.com>
2019-08-06 19:42:08 +0000

    updated dill

--- product_classifier.py
+++ product_classifier.py
@@ -442,47 +442,8 @@ predictions
 
 
 #%%
-'''Save Estimator '''
+''' Save all session variables '''
 
-def serving_input_receiver_fn():
-
-    feature_spec = {
-        "input_ids" : tf.FixedLenFeature([MAX_SEQ_LENGTH], tf.int64),
-        "input_mask" : tf.FixedLenFeature([MAX_SEQ_LENGTH], tf.int64),
-        "segment_ids" : tf.FixedLenFeature([MAX_SEQ_LENGTH], tf.int64),
-        "label_ids" :  tf.FixedLenFeature([], tf.int64)
-    }
-
-    serialized_tf_example = tf.placeholder(dtype=tf.string, shape=[None], name='input_example_tensor')
-    print(serialized_tf_example.shape)
-
-    receiver_tensors = {'example': serialized_tf_example}
-    
-    features = tf.parse_example(serialized_tf_example, feature_spec)
-    
-    return tf.estimator.export.ServingInputReceiver(features, receiver_tensors)
-
-export_path = '/content/drive/My Drive/binary_class/bert/'
-
-estimator._export_to_tpu = False  
-
-estimator.export_saved_model(export_dir_base = export_path, serving_input_receiver_fn = serving_input_receiver_fn)
-
-
-#%%
-''' Dump all session variables '''
 import dill
 
-dill.dump_session('trained_toy_model.db')
-
-#%%
-feature_spec = {
-    input_ids = features["input_ids"]
-    input_mask = features["input_mask"]
-    segment_ids = features["segment_ids"]
-    label_ids = features["label_ids"]
-}
-    
-
-
-#%%
+dill.dump_session('product_classifier.db')
